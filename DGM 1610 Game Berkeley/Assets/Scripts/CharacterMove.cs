c846edea55b6ac3db4e7cd78a99c2716eb43dd65
Berkeley Larkin <lberk14@gmail.com>
2018-09-12 17:23:35 -0600

    Added Character jump button assignment and Ground Check

--- DGM 1610 Game Berkeley/Assets/Scripts/CharacterMove.cs
+++ DGM 1610 Game Berkeley/Assets/Scripts/CharacterMove.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// "All We Have To Do Is Decide What To Do With The Time That Is Given To Us"
+
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int MoveSpeed;
-	public float JumpHeight = 15;
+	public float JumpHeight;
+
+	// Player Grounded Variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
 		print("Hello World");
 	}
 	
+
+	void fixedupdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
+
+	//Wahya Buerre
 	// Update is called once per frame
 	void Update () {
 		
+		// This Code Makes The Character Jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+
+
+	}
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
